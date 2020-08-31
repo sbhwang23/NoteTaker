022539a98302824ef495c2f6c1960887646d562c
Sarah <soobin.sarah.hwang@gmail.com>
2020-08-30 23:42:45 -0700

    working: writeFile

--- Develop/public/assets/routes/apiRoutes.js
+++ Develop/public/assets/routes/apiRoutes.js
@@ -1,15 +1,41 @@
 const fs = require('fs');
+const path = require('path');
+const notes = require('../../../db/db.json');
+
 
 module.exports = function(app) {
+   
+    app.get('/api/notes', (req, res) => {
+        const notes = fs.readFileSync(path.join(__dirname,'../../../db/db.json'), 'utf8');
+        res.json(JSON.parse(notes));
+    });
 
-    app.get('/api/notes', function(req, res) {
-       const notes = fs.readFileSync(path.join(__dirname,'../../../db/db.json'), 'utf8');
-       res.send(JSON.parse(notes));
-    })
+    app.post('/api/notes', (req, res) => {
+        notes.push(req.body);
 
-    // app.post('/api/notes', function(req, res) {
+        fs.writeFileSync(path.join(__dirname,'../../../db/db.json'), JSON.stringify(notes), err => {
+            if (err) {
+            throw err
+            }
+        });
+        res.json(req.body);
+    });
 
-    // })
-
-    // app.delete('/api/notes/:id')
-}
+    app.delete('/api/notes/:id', (req, res) => {
+        const deleteId = req.params.id;
+       
+        notes.forEach((note) => {
+            if (note.id === deleteId) {
+                const noteId = notes.indexOf(note);
+                notes.splice(noteId, 1);
+            }
+        });
+        
+        fs.writeFileSync(path.join(__dirname,'../../../db/db.json'), JSON.stringify(notes), err => {
+            if (err) {
+            throw err
+            }
+        });
+        res.json(notes);
+    });
+}       
--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(__dirname + "/public/assets"));
+app.use(express.static(path.join(__dirname,  "/public")));
 
-require('./assets/routes/apiRoutes')(app);
-require('./assets/routes/htmlRoutes')(app);
+require('./public/assets/routes/apiRoutes')(app);
+require('./public/assets/routes/htmlRoutes')(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
